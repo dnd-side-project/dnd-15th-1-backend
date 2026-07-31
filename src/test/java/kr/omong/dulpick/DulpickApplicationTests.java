85a4cd16e450fcfcf584beafc7590dfd5eb126de
junseok0304 <wicaduce@gmail.com>
2026-07-31 17:06:46 +0900

    fix: Flyway 초기 마이그레이션 기준 보정

--- src/test/java/kr/omong/dulpick/DulpickApplicationTests.java
+++ src/test/java/kr/omong/dulpick/DulpickApplicationTests.java
@@ -1,13 +1,27 @@
 package kr.omong.dulpick;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DulpickApplicationTests {
+
+    @Autowired
+    private Environment environment;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void baselinesExistingSchemaBeforeFirstMigration() {
+        assertThat(environment.getProperty("spring.flyway.baseline-on-migrate"))
+                .isEqualTo("true");
+        assertThat(environment.getProperty("spring.flyway.baseline-version"))
+                .isEqualTo("0");
+    }
 }
